3bf1b630b49fd96aedd3d868c6d5dc742d13109d
Flor Rossi <mfloro.91@gmail.com>
2024-06-16 11:53:43 -0300

    Feat: funcion instalar desde boton

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ const toastMessage = document.querySelector(".toast-body");
 
 // Registro del SW
 
- window.addEventListener(`DOMContentLoaded`, function () {
-if (`serviceWorker` in navigator) {
-    navigator.serviceWorker.register('sw.js')
-        .then((reg) => {
-            toast.show(toastMessage.innerText = "Modo offline activado");
-        })
-        .catch((err) => {
-            
-            toast.show(toastMessage.innerText = "Modo offline no disponible");
-        });
-}
+window.addEventListener(`DOMContentLoaded`, function () {
+    if (`serviceWorker` in navigator) {
+        navigator.serviceWorker.register('sw.js')
+            .then((reg) => {
+                toast.show(toastMessage.innerText = "Modo offline activado");
+            })
+            .catch((err) => {
 
-}); 
+                toast.show(toastMessage.innerText = "Modo offline no disponible");
+            });
+    }
+
+});
 
 // Funcion que optimiza la creación de elementos DOM
 const crearEtiqueta = (nombre = null, atributos = {}, contenido = "") => {
 
-    if (!nombre){
+    if (!nombre) {
         return;
     }
 
@@ -31,7 +31,7 @@ const crearEtiqueta = (nombre = null, atributos = {}, contenido = "") => {
         etiqueta.setAttribute(atributo, atributos[atributo]);
     }
 
-    if (contenido){
+    if (contenido) {
         etiqueta.innerText = contenido;
     }
 
@@ -86,28 +86,28 @@ function mostrarPeliculas(pelicula) {
     let fecha = pelicula["release_date"].slice(0, 4);
     let puntaje = pelicula["vote_average"].toLocaleString('es');
 
-    let divPelis = crearEtiqueta("div", {class: "peli col-6 col-md-4 col-lg-2"});
+    let divPelis = crearEtiqueta("div", { class: "peli col-6 col-md-4 col-lg-2" });
 
-    let divZoom = crearEtiqueta("div", {class: "zoom"});  
+    let divZoom = crearEtiqueta("div", { class: "zoom" });
 
-    let button = crearEtiqueta("button", {class: "bi bi-heart-fill"});
+    let button = crearEtiqueta("button", { class: "bi bi-heart-fill" });
 
-    let img = crearEtiqueta("img", {src: poster, alt: `Poster de ${nombre}`});
+    let img = crearEtiqueta("img", { src: poster, alt: `Poster de ${nombre}` });
 
     // Agregar event listener para redirigir a pelicula.html con el ID de la película
     img.addEventListener("click", () => {
         window.location.href = `pelicula.html?id=${pelicula.id}`;
     });
 
-    let divContainer = crearEtiqueta("div", {class: "container"});
+    let divContainer = crearEtiqueta("div", { class: "container" });
 
-    let divDatos = crearEtiqueta("div", {class: "contenedor-datos-pelis row"});
+    let divDatos = crearEtiqueta("div", { class: "contenedor-datos-pelis row" });
 
-    let h3 = crearEtiqueta("h3", {class: "col-8"}, `${nombre}`);
+    let h3 = crearEtiqueta("h3", { class: "col-8" }, `${nombre}`);
 
-    let p1 = crearEtiqueta("p", {class: "col-4"}, `${fecha}`);
+    let p1 = crearEtiqueta("p", { class: "col-4" }, `${fecha}`);
 
-    let p2 = crearEtiqueta("p", {class: "col-12"}, `Puntaje: ${puntaje}`);
+    let p2 = crearEtiqueta("p", { class: "col-12" }, `Puntaje: ${puntaje}`);
 
     divDatos.append(h3, p1, p2);
     divContainer.append(divDatos);
@@ -116,7 +116,7 @@ function mostrarPeliculas(pelicula) {
     mostrarPeliculas.append(divPelis);
 }
 
-
+// Función buscador
 const busqueda = (datos = []) => {
     const buscarInput = document.getElementById("busqueda");
     const btnBuscar = document.getElementById("button-addon2");
@@ -125,14 +125,51 @@ const busqueda = (datos = []) => {
     const buscar = () => {
         const peliBuscada = buscarInput.value.toLowerCase();
 
-        datos.forEach(pelicula =>{
-            if(pelicula["title"].toLowerCase() == peliBuscada){
+        datos.forEach(pelicula => {
+            if (pelicula["title"].toLowerCase() == peliBuscada) {
                 window.location.href = `pelicula.html?id=${pelicula.id}`;
-            }else{
+            } else {
                 toast.show(toastMessage.innerText = "Película no encontrada");
             }
         });
     }
     buscarInput.addEventListener("search", buscar);
     btnBuscar.addEventListener("click", buscar);
+}
+
+
+
+// Activación del botón instalar
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    console.log("beforeinstallprompt", e)
+    eventoInstalacion = e;
+
+
+    // Traigo botón con DOM e instalo
+    const btnInstalacion = document.querySelector(".btn-flotante");
+
+    btnInstalacion.addEventListener("click", () => {
+        console.log("click en instalar");
+        if (eventoInstalacion && eventoInstalacion.prompt) {
+            eventoInstalacion.prompt()
+                .then((resultado) => {
+                    const opcionElegida = resultado.outcome;
+                    if (opcionElegida == "dismissed") {
+                        toast.show(toastMessage.innerText = "Instalación Cancelada");
+                    } else if (opcionElegida == "accepted") {
+                        toast.show(toastMessage.innerText = "Instalación Completa");
+                        ocultarBtnInstalacion();
+                    }
+                })
+                .catch((error) => console.log("Hubo un error al instalar"))
+        }
+    })
+
+})
+
+// Oculto botón si ya se instaló
+
+const ocultarBtnInstalacion = () => {
+    btnInstalacion.remove();
 }
